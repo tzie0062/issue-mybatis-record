864741e75558720f3d3f86985a8b48cfcf0bb676
Tom Ziemer <tom.ziemer@optile.net>
2023-02-09 15:06:43 +0100

    cleanup

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,7 @@
 package com.example.demo;
 
-import java.util.Optional;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.session.Configuration;
-import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -15,7 +9,5 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-
 
 }
